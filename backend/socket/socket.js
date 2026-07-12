8639d0450bb0825625cb286a27066a28fd750796
Mohit Porwal <mohitpoewal12@gmail.com>
2026-07-13 01:46:48 +0530

    ui updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,14 +6,14 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:5173",
-      "https://real-time-chat-1-4s8o.onrender.com",
-    ],
-    credentials: true,
-  },
+const io = new Server(server,{
+    cors:{
+        origin:[
+            "http://localhost:5173",
+            "https://real-time-chat-1-4s8o.onrender.com"
+        ],
+        credentials:true
+    }
 });
 
 const userSocketMap = {};
